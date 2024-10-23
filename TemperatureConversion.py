091be7e492ba01e4c9b70aa1b6a3fbb734c0ebe9
Aswun <aswinlutfian32@gmail.com>
2024-10-23 11:07:58 +0700

    update

---
+++ TemperatureConversion.py
@@ -0,0 +1,17 @@
+def KonversiSuhu():
+    temperature = float(input("Masukan suhu: "))
+    unit = input("Masukkan unit ('C' atau 'F') : ")
+    if unit.upper() == 'C':
+        fahrenheit = (temperature * 9/5) + 32
+        return f"{temperature}°C = {fahrenheit}°F"
+    elif unit.upper() == 'F':
+        celcius = (temperature - 32) * 5/9
+        return f"{temperature}°F = {celcius}°C"
+    else:
+        print("Unit tidak valid. Gunakan 'C' atau 'F'.")
+
+#CkeF = KonversiSuhu(25, 'c')
+#FkeC = KonversiSuhu(77, 'f')
+#print(f"25 Celsius adalah {CkeF} Fahrenheit")
+#print(f"77 Fahrenheit adalah {FkeC} Celsius")
+print(KonversiSuhu())
